c6647386897ae1b4bfc9100e64d1d77dfc7d9b35
Lester Chan <lesterchan@gmail.com>
2026-05-16 13:56:40 +0800

    Real fakegato-history constructor now loads under a mocked Homebridge v2 HAP API.

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const {
 module.exports = (homebridge) => {
   global.Service = homebridge.hap.Service;
   global.Characteristic = homebridge.hap.Characteristic;
-  global.Formats = homebridge.hap.Formats || homebridge.hap.Characteristic.Formats;
-  global.Perms = homebridge.hap.Perms || homebridge.hap.Characteristic.Perms;
-  global.Units = homebridge.hap.Units || homebridge.hap.Characteristic.Units;
+  global.Formats = homebridge.hap.Formats || Characteristic.Formats;
+  global.Perms = homebridge.hap.Perms || Characteristic.Perms;
+  global.Units = homebridge.hap.Units || Characteristic.Units;
+
+  Characteristic.Formats = Characteristic.Formats || Formats;
+  Characteristic.Perms = Characteristic.Perms || Perms;
+  Characteristic.Units = Characteristic.Units || Units;
+
   // eslint-disable-next-line global-require
   global.FakeGatoHistoryService = require('fakegato-history')(homebridge);
 
