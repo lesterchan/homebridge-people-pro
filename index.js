8fd463bf8c13009f97b41ab3369934cc4386fb83
Lester Chan <lesterchan@gmail.com>
2026-05-16 13:36:45 +0800

    Homebridge V2

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const {
 module.exports = (homebridge) => {
   global.Service = homebridge.hap.Service;
   global.Characteristic = homebridge.hap.Characteristic;
+  global.Formats = homebridge.hap.Formats || homebridge.hap.Characteristic.Formats;
+  global.Perms = homebridge.hap.Perms || homebridge.hap.Characteristic.Perms;
+  global.Units = homebridge.hap.Units || homebridge.hap.Characteristic.Units;
   // eslint-disable-next-line global-require
   global.FakeGatoHistoryService = require('fakegato-history')(homebridge);
 
